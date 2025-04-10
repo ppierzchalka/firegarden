0e977f24410fc7080ee15894be5b95c8fa579182
Przemek Pierzchałka <przemekpierzchalka@gmail.com>
2025-04-10 12:19:58 +0200

    font setup finished

--- apps/firegarden-front/app/fonts.ts
+++ apps/firegarden-front/app/fonts.ts
@@ -1,14 +1,14 @@
-import { LocalFont } from "next/font/local";
+import localFont from "next/font/local";
 
 // Load Geist Sans font
-export const geistSans = LocalFont({
+export const geistSans = localFont({
 	src: "./fonts/GeistVF.woff",
 	variable: "--font-geist-sans",
 	display: "swap",
 });
 
 // Load Geist Mono font
-export const geistMono = LocalFont({
+export const geistMono = localFont({
 	src: "./fonts/GeistMonoVF.woff",
 	variable: "--font-geist-mono",
 	display: "swap",
