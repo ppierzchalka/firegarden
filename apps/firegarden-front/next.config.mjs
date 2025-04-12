553371c9567beb537ce9af04cc68f8625c0000c4
Przemysław Pierzchałka <przemekpierzchalka@gmail.com>
2025-04-13 01:10:21 +0200

    another attempt of configuring netlify via toml file

--- apps/firegarden-front/next.config.mjs
+++ apps/firegarden-front/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		unoptimized: true, // Disables Next.js Image Optimization API
+	},
+};
 
 export default nextConfig;
