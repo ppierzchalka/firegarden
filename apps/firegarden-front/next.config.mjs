b5e16e512be223037d06dcfc22c4ecc862ebfd1f
Przemysław Pierzchałka <przemekpierzchalka@gmail.com>
2025-06-23 00:02:47 +0200

    build from docker (#19)

--- apps/firegarden-front/next.config.mjs
+++ apps/firegarden-front/next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
 	images: {
 		unoptimized: true, // Disables Next.js Image Optimization API
 	},
+	output: "standalone", // Optimizes for Docker deployments
 };
 
 export default nextConfig;
